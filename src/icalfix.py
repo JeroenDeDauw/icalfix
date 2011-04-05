214a1ce1913d4d46bc91ec99e65adec61c9290d4
Jeroen De Dauw <jeroendedauw@gmail.com>
2011-04-05 23:19:02 +0200

    implemented, only need to hold into account month edges

--- src/icalfix.py
+++ src/icalfix.py
@@ -10,12 +10,46 @@ class IcalTimezoneFix(object):
     '''
     
     def fix(self, ical, offset=2):
-        pass
+        return '\n'.join([self.fixLine(line, offset) for line in ical.split('\n')])
+    
+    def fixLine(self, line, offset):
+        if ['DTSTART', 'DTEND'].count(line.split(':')[0]) == 1:
+            parts = line.split(':')
+            parts[1] = self.fixTime(parts[1], offset)
+            line = ':'.join(parts)
+        return line
+    
+    def fixTime(self, fulltime, offset):
+        parts = fulltime.split('T')
+        
+        if len(parts) == 1:
+            return fulltime
+        
+        date = parts[0]
+        hour = int(parts[1][0:2])
+        minutes = parts[1][2:] 
+        
+        hour += offset
+        
+        if hour > 23:
+            dateoffset = 1
+        elif hour < 0:
+            dateoffset = -1
+        else:
+            dateoffset = 0
+        
+        hour += 24 * dateoffset
+        
+        if dateoffset != 0:
+            # TODO: obviously this fails at month edges; fix
+            date = date[:-2] + str(int(date[-2:]) + dateoffset)
+        
+        return date + 'T' + str(hour).rjust(2,'0') + minutes + ' - ' + fulltime
 
 def main():
     fixer = IcalTimezoneFix()
-    f = open('/tmp/workfile', 'r')
+    f = open('demo.ics', 'r')
     print fixer.fix(f.read())
 
 if __name__ == '__main__':
-    main
+    main()
